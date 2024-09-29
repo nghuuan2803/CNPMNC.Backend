da9301aea50b6a41c4e74965d962d242a99d4399
huuan28 <nghuuan2803@gmail.com>
2024-09-29 10:52:53 +0700

    abcxyz

--- WMS.Infrastructure/Repositories/ProductGroup/CategoryRepository.cs
+++ WMS.Infrastructure/Repositories/ProductGroup/CategoryRepository.cs
@@ -9,12 +9,5 @@ namespace WMS.Infrastructure.Repositories.ProductData
     public class CategoryRepository : BaseRepository<Category, int>, ICategoryRepository
     {
         public CategoryRepository(AppDbContext db) : base(db) { }
-
-        public override async Task<IEnumerable<Category>> GetListAsync(Expression<Func<Category, bool>> predicate)
-        {
-            if (predicate == null)
-                return await _db.Categories.Include(i => i.Products).ToListAsync();
-            return await _db.Categories.Where(predicate).Include(i => i.Products).ToListAsync();
-        }
     }
 }
